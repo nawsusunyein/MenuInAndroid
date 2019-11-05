084d2615406969024ed1812e345a9bf3f229b668
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-05 12:02:40 +0630

    insert context menu and action in activity

--- app/src/main/java/com/example/menutest/MainActivity.java
+++ app/src/main/java/com/example/menutest/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.menutest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnShowContext = (Button) findViewById(R.id.btnShowContextMenu);
+        registerForContextMenu(btnShowContext);
     }
 
+
+    // Creating options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.options_menu,menu);
@@ -41,6 +48,23 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //Creating context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+        menu.setHeaderTitle("Actions Available");
+        menu.add(0,v.getId(),0,"Delete");
+        menu.add(0,v.getId(),0,"Top");
+        menu.add(0,v.getId(),0,"Unread");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+
+        Toast.makeText(MainActivity.this,item.getTitle() + " is selected.",Toast.LENGTH_LONG).show();
+        return true;
     }
 
 }
